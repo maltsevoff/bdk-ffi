f53172251660cc7523d44458047ec7d9d5236a0a
maltsevoff <alexmalczev@gmail.com>
2025-12-14 19:03:54 +0200

    refactor folder path

--- bdk-swift/build-xcframework.sh
+++ bdk-swift/build-xcframework.sh
@@ -10,7 +10,7 @@ OUTDIR="."
 RELDIR="release-smaller"
 NAME="bdkffi"
 STATIC_LIB_NAME="lib${NAME}.a"
-NEW_HEADER_DIR="../bdk-ffi/target/include"
+NEW_HEADER_DIR="../bdk-ffi/target/include/BitcoinDevKitFFI"
 
 cd ../bdk-ffi/ || exit
 
@@ -47,20 +47,6 @@ cd ../bdk-swift/ || exit
 #       BitcoinDevKitFFI.h
 #       module.modulemap
 
-# Start from a clean header include dir so we don't get duplicates
-rm -f "${NEW_HEADER_DIR}/BitcoinDevKitFFI.h" "${NEW_HEADER_DIR}/module.modulemap"
-rm -rf "${NEW_HEADER_DIR}/BitcoinDevKitFFI"
-mkdir -p "${NEW_HEADER_DIR}/BitcoinDevKitFFI"
-
-# Move generated header and modulemap into BitcoinDevKitFFI subfolder only
-mv "${HEADERPATH}" "${NEW_HEADER_DIR}/BitcoinDevKitFFI/BitcoinDevKitFFI.h"
-mv "${MODMAPPATH}" "${NEW_HEADER_DIR}/BitcoinDevKitFFI/module.modulemap"
-
-# Ensure the modulemap points at the header using the BitcoinDevKitFFI/ prefix,
-# matching the desired structure inside bdkffi.xcframework.
-sed -i '' 's#header \"BitcoinDevKitFFI/BitcoinDevKitFFI.h\"#header \"BitcoinDevKitFFI.h\"#' "${NEW_HEADER_DIR}/BitcoinDevKitFFI/module.modulemap" || true
-
-echo -e "\n" >> "${NEW_HEADER_DIR}/BitcoinDevKitFFI/module.modulemap"
 # remove old xcframework directory
 rm -rf "${OUTDIR}/${NAME}.xcframework"
 
